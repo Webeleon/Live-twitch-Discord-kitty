475490349b9efa3f09d895f9c1734309262a943f
Julien Prugne <julien@webeleon.dev>
2022-02-15 07:55:07 +0100

    ci: add clevercloud env vars for postgres addon

--- src/configurations/database.config.ts
+++ src/configurations/database.config.ts
@@ -2,9 +2,18 @@ import { registerAs } from '@nestjs/config';
 
 export const databaseConfig = registerAs('database', () => ({
   type: process.env.DB_TYPE || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME || 'discord_kitty',
-  password: process.env.DB_PASSWORD || 'discord_kitty',
-  database: process.env.DB_DATABASE || 'discord_kitty',
+  host: process.env.DB_HOST || process.env.POSTGRESQL_ADDON_HOST || 'localhost',
+  port: process.env.DB_PORT || process.env.POSTGRESQL_ADDON_PORT || 5432,
+  username:
+    process.env.DB_USERNAME ||
+    process.env.POSTGRESQL_ADDON_USER ||
+    'discord_kitty',
+  password:
+    process.env.DB_PASSWORD ||
+    process.env.POSTGRESQL_ADDON_PASSWORD ||
+    'discord_kitty',
+  database:
+    process.env.DB_DATABASE ||
+    process.env.POSTGRESQL_ADDON_DB ||
+    'discord_kitty',
 }));
