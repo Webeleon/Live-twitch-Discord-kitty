6d3b3ee75208404caa4b8babccb0126b053a66d4
Julien Prugne <julien@webeleon.dev>
2022-02-26 23:12:05 +0100

    fix: commands import path

--- src/discord/commands/commands.module.ts
+++ src/discord/commands/commands.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommandsService } from './commands.service';
-import { StartHandler } from './handlers/start.handler';
+import { StartHandler } from './handlers/start/start.handler';
 import { KittyModule } from '../../kitty/kitty.module';
 import { UserModule } from '../../user/user.module';
-import { ListHandler } from './handlers/list.handler';
+import { ListHandler } from './handlers/list/list.handler';
 
 @Module({
   imports: [KittyModule, UserModule],
